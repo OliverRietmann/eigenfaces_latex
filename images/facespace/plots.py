b95d1286ad19ba261defaba7c9eb4cca0ee16dad
Oliver Rietmann <rioliver@student.ethz.ch>
2021-07-12 21:43:07 +0200

    Plot with components in [0,1]

--- images/facespace/plots.py
+++ images/facespace/plots.py
@@ -1,46 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(0)
-
-m = np.array([2.0, 6.0])
+np.random.seed(1)
 
 phi = 0.0
 R = lambda phi: np.array([[np.cos(phi), -np.sin(phi)], [np.sin(phi), np.cos(phi)]])
-d = 5.0
+d = 10.0
 D = np.array([[d, 0.0], [0.0, 1.0 / d]])
 
-x = np.random.normal(0.0, 1.0, 50)
-y = np.random.normal(0.0, 1.0, 50)
+d_inv = 1.0 / d
+#x = np.random.uniform(d_inv, 1.0 - d_inv, 50)
+#y = np.random.uniform(d_inv, 1.0 - d_inv, 50)
+x = np.random.normal(0.55, 0.25 * d_inv, 50)
+y = np.random.normal(0.4, 0.25 * d_inv, 50)
+m = np.array([np.mean(x), np.mean(y)])
 
-angle = 0.5
+
+angle = -0.2 * np.pi
 A = R(angle) @ D @ R(-angle)
 
 Z = np.empty((2, len(x)))
-Z[0] = x
-Z[1] = y
+Z[0] = x - m[0]
+Z[1] = y - m[1]
 
 Z_tilde = A @ Z
 
-x_tilde = Z_tilde[0]
-y_tilde = Z_tilde[1]
+x_tilde = Z_tilde[0] + m[0]
+y_tilde = Z_tilde[1] + m[1]
+#m = np.array([np.mean(x_tilde), np.mean(y_tilde)])
 
 w = np.array([np.cos(angle), np.sin(angle)]) / np.cos(angle)
-l = np.linspace(-12, 13, 10)
+l = np.linspace(-1.2, 1.2, 10)
 v = np.empty((2, len(l)))
 v[0] = l * w[0]
 v[1] = l * w[1]
 
 plt.figure()
 #plt.plot(v[0] + m[0], v[1] + m[1], 'y-', label=r"Affiner Unterraum der Gesichter")
-plt.plot(x_tilde + m[0], y_tilde + m[1], 'g.', label=r"Gesichter $b_k$")
+plt.axhline(y=0, color='k')
+plt.axvline(x=0, color='k')
+plt.plot(x_tilde, y_tilde, 'g.', label=r"Gesichter $\vec{b}_k$")
 plt.plot(v[0], v[1], 'c-', label=r"Unterraum der Differenzgesichter")
-plt.plot(x_tilde, y_tilde, 'b.', label=r"Differenzgesichter $a_k$")
-plt.plot(m[0], m[1], 'r.', label=r"Durchschnittsgesicht $m$")
-plt.xlim(-12.0, 12.0)
-plt.xticks(range(-12, 13, 2))
-plt.yticks(range(-6, 12, 2))
-plt.legend(loc='lower right')
+plt.plot(x_tilde - m[0], y_tilde - m[1], 'b.', label=r"Differenzgesichter $\vec{a}_k$")
+plt.plot(m[0], m[1], 'r.', label=r"Durchschnittsgesicht $\vec{m}$")
+plt.xlim(-1.1, 1.1)
+plt.ylim(-1.1, 1.1)
+#plt.xticks(range(-12, 13, 2))
+#plt.yticks(range(-6, 12, 2))
+plt.legend(loc='lower left')
 plt.gca().set_aspect('equal', adjustable='box')
 plt.grid('True')
 plt.tight_layout()
